c59fd5203ed6bd6086e0041e7bcdb9c04d420e5c
Tran Bao Tin <tantrin674@gmail.com>
2024-07-07 22:44:42 +0700

    add all snake game 1 code done

--- Game 1/Game1.cpp
+++ Game 1/Game1.cpp
@@ -55,18 +55,151 @@ void GameRender(string playerName)
 			// Creating the snake's food with '#'
 			else if (i == fruitCordY && j == fruitCordX)
 				cout << '#';
-			// Creating snake's head with 'O'
+			// Creating snake's body with 'o'
 			else {
 				bool prTail = false;
 				for (int k = 0; k < snakeTailLen; k++) {
 					if (snakeTailX[k] == j
 						&& snakeTailY[k] == i) {
-						cout << 'O';
+						cout << 'o';
 						prTail = true;
 					}
 				}
+				if (!prTail)
+					cout << ' ';
 			}
 		}
+		cout << endl;
 	}
 
+	// Creating bottom walls with '-'
+	for (int i = 0; i < width + 2; i++)
+		cout << '-';
+	cout << endl;
+
+	// Display player's score
+	cout << playerName << "'s Score: " << playerScore
+		<< endl;
+}
+
+// Function for updating the game state
+void UpdateGame()
+{
+	int prevX = snakeTailX[0];
+	int prevY = snakeTailY[0];
+	int prev2X, prev2Y;
+	snakeTailX[0] = x;
+	snakeTailY[0] = y;
+
+	for (int i = 1, i < snakeTailLen; i++) {
+		prev2X = snakeTailX[i];
+		prev2Y = snakeTailY[i];
+		snakeTailX[i] = prevX;
+		snakeTailY[i] = prevY;
+	}
+
+	switch (sDir) {
+	case LEFT:
+		x--;
+		break;
+	case RIGHT:
+		x++;
+		break;
+	case UP:
+		y--;
+		break;
+	case DOWN:
+		y++;
+		break;
+	}
+
+	// Checks for snake's collision with the wall (|)
+	if (x >= width || x < 0 || y >= height || y < 0)
+		isGameOver = true;
+
+	// Check for collision with the tail (o)
+	for (int i = 0; i < snakeTailLen; i++) {
+		if (snakeTailX[i] == x && snakeTailY[i] == y)
+			isGameOver = true;
+	}
+
+	// Check for snake's collision with the food (#)
+	if (x == fruitCordX && y == fruitCordY) {
+		playerScore += 10;
+		fruitCordX = rand() % width;
+		fruitCordY = rand() % height;
+		snakeTailLen++;
+	}
+}
+
+// Function to set the difficulty for the game level
+int SetDifficulty() 
+{
+	int dfc, choice;
+	cout << "\nSET DIFFICULTY\n1: Easy\n2: Medium\n3: Hard "
+		"\nNOTE: if not chosen or pressed any other "
+		"key, the difficulty will be automatically set "
+		"to Medium\nChoose difficulty level: ";
+	cin >> choice;
+	switch (choice) {
+	case '1':
+		dfc = 50;
+		break;
+	case '2':
+		dfc = 100;
+		break;
+	case '3':
+		dfc = 150;
+		break;
+	default:
+		dfc = 100;
+	}
+	return dfc;
+}
+
+//Function to handle user input
+void UserInput() 
+{
+	// Check if a key is pressed or not
+	if (_kbhit()) {
+		// get the pressed key
+		switch (_getch()) {
+		case 'a':
+			sDir = LEFT;
+			break;
+		case'd':
+			sDir = RIGHT;
+			break;
+		case 'w':
+			sDir = UP;
+			break;
+		case 's':
+			sDir = DOWN;
+			break;
+		case'p':
+			isGameOver = true;
+			break;
+		}
+	}
+}
+
+
+// Main function  / game looping function
+int main()
+{
+	string playerName;
+	cout << 'enter your name: ';
+	cin >> playerName;
+	int dfc == SetDifficulty();
+
+	GameInit();
+	while (!isGameOver) {
+		GameRender(playerName);
+		UserInput();
+		UpdateGame();
+		//Create a delay for according to the chosen difficulty
+		Sleep(dfc);
+	}
+
+	return 0;
 }
